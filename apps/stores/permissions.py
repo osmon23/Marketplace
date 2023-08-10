6544c0791db6a93010025e7f50d51a482933a655
Dastan <dastiw1010@gmail.com>
2023-08-10 18:52:18 +0600

    Fixed serializers, Views

--- apps/stores/permissions.py
+++ apps/stores/permissions.py
@@ -1,6 +1,6 @@
 from rest_framework import permissions
 
-from .models import Product, Store
+from apps.stores.models import Store, Product
 
 
 class IsAdminOrSeller(permissions.BasePermission):
@@ -8,9 +8,12 @@ class IsAdminOrSeller(permissions.BasePermission):
         return request.user and (request.user.is_superuser or request.user.role == 'Seller')
 
     def has_object_permission(self, request, view, obj):
-        if request.user.is_superuser or request.user.role == 'Seller':
-            if isinstance(obj, Store):
-                return obj.seller == request.user
-            elif isinstance(obj, Product):
-                return obj.store.seller == request.user
+        if request.user.is_authenticated:
+            if request.method in permissions.SAFE_METHODS:
+                return True
+            if request.user.is_superuser or request.user.role == 'Seller':
+                if isinstance(obj, Store):
+                    return obj.seller == request.user
+                elif isinstance(obj, Product):
+                    return obj.store.seller == request.user
         return False
--- apps/stores/views.py
+++ apps/stores/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 from .models import Product, Store
 from .serializers import ProductSerializer, StoreSerializer
@@ -8,11 +8,11 @@ from .permissions import IsAdminOrSeller
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [IsAdminOrSeller]
+    permission_classes = [IsAdminOrSeller, permissions.IsAuthenticatedOrReadOnly]
 
 
 class StoreViewSet(viewsets.ModelViewSet):
     queryset = Store.objects.all()
     serializer_class = StoreSerializer
-    permission_classes = [IsAdminOrSeller]
+    permission_classes = [IsAdminOrSeller, permissions.IsAuthenticatedOrReadOnly]
 
