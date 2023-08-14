13cdf41439ae2933e63303dd76eac319346d9d48
Dastan-Oskonbaev <dastiw1910gmail.com>
2023-08-14 18:25:43 +0600

    Added reviews for products

--- apps/stores/serializers.py
+++ apps/stores/serializers.py
@@ -1,5 +1,44 @@
-from .models import Specifications, ProductImage, Product, Store
+from .models import Specifications, ProductImage, Product, Store, Review
 from rest_framework import serializers
+
+
+
+class FilterReviewListSerializer(serializers.ListSerializer):
+    def to_representation(self, data):
+        data = data.filter(parent=None)
+        return super().to_representation(data)
+
+
+class RecursiveSerializer(serializers.Serializer):
+    def to_representation(self, value):
+        serializer = self.parent.parent.__class__(value, context=self.context)
+        return serializer.data
+
+
+class ReviewCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Review
+        fields = "__all__"
+        extra_kwargs = {
+            'parent': {'required': False}
+        }
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        validated_data['name'] = user.get_full_name() or user.username
+        validated_data['email'] = user.email
+        review = Review.objects.create(**validated_data)
+        return review
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    children = RecursiveSerializer(many=True)
+
+    class Meta:
+        list_serializer_class = FilterReviewListSerializer
+        model = Review
+        fields = ("id", "name", "text", "children")
 
 
 class SpecificationsSerializer(serializers.ModelSerializer):
@@ -24,6 +63,7 @@ class ProductImageSerializer(serializers.ModelSerializer):
 class ProductSerializer(serializers.ModelSerializer):
     images = ProductImageSerializer(many=True, read_only=True)
     specifications = SpecificationsSerializer(many=True, read_only=True)
+    reviews = ReviewSerializer(many=True, read_only=True)
 
     class Meta:
         model = Product
@@ -35,6 +75,7 @@ class ProductSerializer(serializers.ModelSerializer):
             'quantity',
             'images',
             'specifications',
+            'reviews',
         )
 
 
