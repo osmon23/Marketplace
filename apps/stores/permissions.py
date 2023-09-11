e1a90433c448ab133636f9d22f98d8825b17fa3d
Dastan-Oskonbaev <dastiw1910@gmail.com>
2023-09-11 12:26:20 +0600

    Fixed permissions

--- apps/stores/permissions.py
+++ apps/stores/permissions.py
@@ -5,6 +5,8 @@ from apps.stores.models import Store, Product
 
 class IsAdminOrSeller(permissions.BasePermission):
     def has_permission(self, request, view):
+        if request.method == 'GET':
+            return True
         return request.user and (request.user.is_staff or request.user.role == 'Seller')
 
     def has_object_permission(self, request, view, obj):
