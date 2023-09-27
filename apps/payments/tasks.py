02fc406c1a37a30544a43986b0b7734598019d36
Dastan-Oskonbaev <dastiw1910@gmail.com>
2023-09-27 16:29:51 +0600

    Added task for celery(clean expired tariffs)

--- apps/payments/tasks.py
+++ apps/payments/tasks.py
@@ -1,23 +1,23 @@
 from datetime import date
 
 from celery import shared_task
-from apps.payments.models import Payment
+from apps.payments.models import TariffPayment
 from utils.time import get_current_date
 
 
-# @shared_task
-# def clear_subscriptions():
-#     today = get_current_date()
-#     expired_payments = Payment.objects.filter(end_date__lt=today, is_active=True)
-#
-#     for payment in expired_payments:
-#         payment = payment.product
-#
-#         payment.is_active = False
-#         payment.end_date = today
-#         payment.save()
-#
-#
+@shared_task
+def clear_tariffs():
+    today = get_current_date()
+    expired_tariffs = TariffPayment.objects.filter(end_date__lt=today, is_active=True)
+
+    for tariff in expired_tariffs:
+        store = tariff.store
+        store.product_limit = 10
+        store.product.range_weight = 0
+        store.save()
+        tariff.is_active = False
+        tariff.save()
+
 # @shared_task
 # def activate_payment():
 #     today = get_current_date()
